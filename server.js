0f91f9192fef6b9885b8dbcdbebff7c82c8f3fa3
aktasevren <evrenaktas@yahoo.com>
2023-06-30 09:31:38 +0300

    origin update

--- server.js
+++ server.js
@@ -9,10 +9,30 @@ const port = 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(
     cors({
-        origin: ["http://localhost:3001", "http://localhost:3000","https://filming-location-finder.vercel.app/"],
+        origin: ["http://localhost:3001", "http://localhost:3000","https://filming-location-finder.vercel.app"],
         credentials: true,
+        
     })
 );
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://filming-location-finder.vercel.app');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.get('/', (req, res) => {
     res.send('Hi, Filming Location Finder App /n Example : "https://www.locahost:3000/imdbid/tt12345678"');
