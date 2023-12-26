ad9935871e318f661e739da44ff28968636d75c6
aktasevren <evrenaktas@yahoo.com>
2023-12-26 13:59:15 +0300

    new

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const app = express();
 const cors = require("cors");
 
-const port = 5000;
+const port = 8080;
 
 
 app.use(express.urlencoded({ extended: false }));
