ab5ea6621bb36340970952baab4928b2c949595d
aktasevren <evrenaktas@yahoo.com>
2024-01-06 18:42:24 +0300

    new version v24.0

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const cors = require("cors");
 
 const port = 3000;
 
-
 app.use(express.urlencoded({ extended: false }));
 app.use(
     cors({
@@ -16,10 +15,10 @@ app.use(
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
+    return next();
 });
 
 app.get('/', (req, res) => {
@@ -34,25 +33,28 @@ app.get('/', (req, res) => {
 })
 
 app.get('/imdbid/:id', (req, res) => {
+    const start = performance.now();
     const imdbid = req.params.id
-
-    console.log(`Request received for ${imdbid}`)
-
     axios.get(`https://caching.graphql.imdb.com/?operationName=TitleFilmingLocationsPaginated&variables=%7B%22after%22%3A%22bGMwMjkwODcz%22%2C%22const%22%3A%22${imdbid}%22%2C%22first%22%3A50%2C%22isAutoTranslationEnabled%22%3Afalse%2C%22locale%22%3A%22en-US%22%2C%22originalTitleText%22%3Afalse%7D&extensions=%7B%22persistedQuery%22%3A%7B%22sha256Hash%22%3A%22f8e058a92242ae7940109b6669584768279c3e874ad37d0a792ad24f43627501%22%2C%22version%22%3A1%7D%7D`,
         {
             headers: {
                 'Content-Type': 'application/json'
             }
         })
-
         .then((response) => {
             if (response.data.data.title == null) {
-                res.json("Please check sent id value")
+                res.json("no such imdb id was found")
             } else {
+                console.log(response.data.data.title.filmingLocations.edges)
                 const locs = response.data.data.title.filmingLocations.edges;
                 const locations = [];
                 locs.map((loc) => locations.push(loc.node.text));
-                res.status(200).json(locations)
+                const end = performance.now();
+                res.status(200).json({
+                    "imdbid": imdbid,
+                    "locations": locations,
+                    "runtime": end - start,
+                })
             }
         })
         .catch(function (error) {
@@ -60,10 +62,8 @@ app.get('/imdbid/:id', (req, res) => {
         });
 })
 app.listen(port, () => {
-    console.log(`Filming Location Application Started`)
+    console.log(`filming locations API started on port = ${port}`)
 });
-
-
 
 module.exports = app;
 
